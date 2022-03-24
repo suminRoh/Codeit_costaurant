19c092695b2d4e7855e8781c323ad3b48130a2b5
suminRoh <roosm@naver.com>
2022-03-24 20:54:06 +0900

    Django study 3/24

--- costaurant/settings.py
+++ costaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vdvn_1ne)aoh*t#hy6_o-n7pcm*8%bup^2$=0^l&!8ew4(+75%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
--- foods/views.py
+++ foods/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
+from foods.models import Menu
 
 # Create your views here.
 def index(request):
+    context=dict()
     today=datetime.today().date()
-    context={"date":today}
+    menus=Menu.objects.all()  
+    context["date"]=today
+    context["menus"]=menus
     return render(request,'foods/index.html',context=context)
 
-def food_detail(request,food):
-    context={'name':food}
+def food_detail(request,pk):
+    context=dict()
+    menu=Menu.objects.get(id=pk)
+    context["menu"]=menu
     return render(request,'foods/detail.html',context=context)
+
+ 
